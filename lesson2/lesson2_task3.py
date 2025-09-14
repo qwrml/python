212a567c49e21202cc345a48db052a0564fa113b
qwrml <yhimh.yhimh@yandex.ru>
2025-09-14 19:37:41 +0500

    upd 2

--- lesson2/lesson2_task3.py
+++ lesson2/lesson2_task3.py
@@ -1,9 +1,10 @@
+import math
 def square(side):
     area = side * side
     if side == int(side):
         return area
     else:
-        return int(area) + 1
+        return math.ceil(area)
 print(square(5))
 print(square(3.2))
 print(square(4.7))
